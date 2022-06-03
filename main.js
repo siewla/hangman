ffd36a51e27713eb71f4e36bfd738cdcdb7b7312
siewla <siewlapang@gmail.com>
2022-06-03 15:51:10 +0800

    completed hangman no css

---
+++ main.js
@@ -0,0 +1,112 @@
+class Hangman {
+    static WORDS = [
+        "country",
+        "recipe",
+        "mud",
+        "application",
+        "girl",
+        "success",
+        "argument",
+        "dad",
+        "recommendation",
+        "attention"
+    ]
+
+    constructor(maxNoOfGuesses = 10) {
+        this.maxNoOfGuesses = maxNoOfGuesses
+        this.randomWord = ''
+        this.guessedAlphabets = []
+        this.isFound = false
+        this.counter = 0
+    }
+
+    getRandomWord() {
+        const randomWord = Hangman.WORDS[Math.floor(Math.random() * Hangman.WORDS.length)]
+        this.randomWord = new Word(randomWord)
+    }
+
+    resetGame() {
+        this.isFound = false
+        this.randomWord = ''
+        this.guessedAlphabets = []
+        this.counter = 0
+    }
+
+    startGame() {
+        this.getRandomWord()
+        const displayEl = document.querySelector('.display')
+        displayEl.innerHTML = this.randomWord.display()
+        const guessedAlphabetForm = document.querySelector('#alphabet-box')
+        const guessedAlphabetEl = document.querySelector('#guess-alphabet')
+        const previousGuessesEl = document.querySelector('.previous-guesses')
+        guessedAlphabetForm.onsubmit = (e) => {
+            e.preventDefault()
+            this.randomWord.checkLetterIsFound(guessedAlphabetEl.value)
+            this.guessedAlphabets.push(guessedAlphabetEl.value)
+            displayEl.innerHTML = this.randomWord.display()
+            guessedAlphabetEl.value = ''
+            this.counter++;
+            previousGuessesEl.innerHTML = this.guessedAlphabets.join(' ')
+            const wordIsFound = this.randomWord.wordIsFound()
+
+            if (this.counter > this.maxNoOfGuesses && !wordIsFound) {
+                this.gameOver()
+            }
+
+            if (wordIsFound) {
+                this.winGame()
+            }
+        }
+    }
+
+    gameOver() {
+        alert('you lose')
+        this.resetGame()
+    }
+
+    winGame() {
+        alert('you win')
+        this.resetGame()
+    }
+}
+
+class Word {
+    constructor(value = '') {
+        this.value = value
+        this.isFound = false
+        this.letters = this.value
+            .split('')
+            .map(letter => new Letter(letter))
+    }
+
+    wordIsFound() {
+        return this.letters.every(letter => letter.isFound)
+    }
+
+    checkLetterIsFound(guessedAlphabet = '') {
+        this.letters.forEach(letter => letter.checkLetterIsFound(guessedAlphabet))
+    }
+
+    display() {
+        return this.letters
+            .map(letter => letter.isFound ? letter.value : '_')
+            .join(' ')
+
+    }
+}
+
+class Letter {
+    constructor(value = '') {
+        this.value = value
+        this.isFound = false
+    }
+
+    checkLetterIsFound(guessedAlphabet) {
+        if (guessedAlphabet === this.value) {
+            this.isFound = true
+        }
+    }
+}
+
+const newGame = new Hangman(10)
+newGame.startGame()
